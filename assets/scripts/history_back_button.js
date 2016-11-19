52d1008bc027d19d60a488196f73179e29efcec9
louisvno <louisvn@gmail.com>
2016-11-19 10:33:29 +0100

    first commit

---
+++ assets/scripts/history_back_button.js
@@ -0,0 +1,5 @@
+document.getElementById("back-button").addEventListener("click",goBack);
+
+function goBack() {
+    window.history.back();
+}
---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+$('#select-by-team').change(function () {
+  this.form.submit();
+});
+
+$('#select-by-date').change(function () {
+  this.form.submit();
+});
+
